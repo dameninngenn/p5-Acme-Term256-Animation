012654b875a7b8182137a73e9500c1a47ea61ec6
dameninngenn <dameninngenn.owata@gmail.com>
2012-07-25 18:24:59 +0900

    initial commit

---
+++ lib/Acme/Term256/Animation.pm
@@ -0,0 +1,32 @@
+package Acme::Term256::Animation;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Term256::Animation -
+
+=head1 SYNOPSIS
+
+  use Acme::Term256::Animation;
+
+=head1 DESCRIPTION
+
+Acme::Term256::Animation is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ xt/01_podspell.t
@@ -0,0 +1,10 @@
+use Test::More;
+eval q{ use Test::Spelling };
+plan skip_all => "Test::Spelling is not installed." if $@;
+add_stopwords(map { split /[\s\:\-]/ } <DATA>);
+$ENV{LANG} = 'C';
+all_pod_files_spelling_ok('lib');
+__DATA__
+dameninngenn
+dameninngenn.owata {at} gmail.com
+Acme::Term256::Animation
